8e8fb5769b47b52eadbd4b0984d3c87f14735041
Alejandro Trucco <alejandrotrucco29@gmail.com>
2024-03-01 01:01:45 -0300

    test: pipeline and test

--- demo/settings.py
+++ demo/settings.py
@@ -1,6 +1,6 @@
 import environ
 from pathlib import Path
-from api
+import api
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,6 +8,7 @@ env = environ.Env()
 env.read_env(BASE_DIR / '.env')
 
 
+ os.environ.setdefault('DJANGO_SETTINGS_MODULE', '.settings')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
