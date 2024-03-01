ea251930aaffb4d72b43a13d51cb2308ad42881a
Alejandro Trucco <alejandrotrucco29@gmail.com>
2024-03-01 16:11:47 -0300

    test: test en ci-cd

--- demo/settings.py
+++ demo/settings.py
@@ -92,6 +92,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+STATIC_URL = 'static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
